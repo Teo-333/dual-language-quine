23ffb5b1e71e4abfe289950c5f90926e997d39b9
Fedor Rushkevich <fedrushkevich@gmail.com>
2025-03-13 17:23:17 +0200

    (dlq-m): add solution (index.js)

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function quine() {
-  console.log(quine.toString() + " quine();");
+function DoubleQuine() {
+  let jsCode = `function DoubleQuine() {
+    let csCode = \`using System;
+    class Program {
+        static void Main() {
+            string jsCode = @\\"function DoubleQuine() {\\\\n    let csCode = \\\\\\\"...\";
+            console.log(csCode.replace(/\\\\/g, ""\\\\\\").replace(/"/g, '\\\\\\"'));
+        }
+    }\`;
+    console.log(csCode);
+}`;
+
+  let csCode = `using System;
+class Program {
+    static void Main() {
+        string jsCode = @"${jsCode.replace(/\\/g, "\\\\").replace(/"/g, '""')}";
+        Console.WriteLine(jsCode);
+    }
+}`;
+
+  console.log(csCode);
 }
-quine();
+
+DoubleQuine();
